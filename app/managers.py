dda933281ac4ee92cae8f36af88f22c4ce79957f
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-05-28 14:10:15 +0300

    solved

--- app/managers.py
+++ app/managers.py
@@ -4,19 +4,19 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.conn = sqlite3.connect(db_name)
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> None:
         self.conn.execute(
             f"INSERT INTO {self.table_name} VALUES (?, ?)",
             (first_name, last_name),
         )
         self.conn.commit()
 
-    def all(self):
+    def all(self) -> list[Actor]:
         db_select = self.conn.execute(
             f"SELECT * FROM {self.table_name}"
         )
@@ -24,7 +24,7 @@ class ActorManager:
             Actor(*row) for row in db_select
         ]
 
-    def update(self, pk: int, new_first_name: str, new_last_name: str):
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.conn.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name=?, "
@@ -34,8 +34,9 @@ class ActorManager:
         )
         self.conn.commit()
 
-    def delete(self, pk: int):
+    def delete(self, pk: int) -> None:
         self.conn.execute(
             f"DELETE FROM {self.table_name} WHERE id=? ",
             (pk,)
         )
+        self.conn.commit()
